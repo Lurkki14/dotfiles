ac9dc9f555d35825deea5d252ed611c35d811553
Jussi Kuokkanen <jussi.kuokkanen@protonmail.com>
2022-08-24 17:26:31 +0300

    pin nixpkgs when calling nixos-rebuild

--- rebuild.sh
+++ rebuild.sh
@@ -1,1 +1,5 @@
-sudo nixos-rebuild switch -I nixos-config=configuration.nix
+pinnedCommit="6c6409e965a6c883677be7b9d87a95fab6c3472e"
+nixpkgsUrlFromCommit="https://github.com/NixOS/nixpkgs/archive/$pinnedCommit.tar.gz"
+
+
+NIX_PATH="nixpkgs=$nixpkgsUrlFromCommit" sudo -E nixos-rebuild switch -I nixos-config=configuration.nix
