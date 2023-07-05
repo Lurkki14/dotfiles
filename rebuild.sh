850f22030a698b2fbf23443835382b5d94580067
Jussi Kuokkanen <jussi.kuokkanen@protonmail.com>
2023-07-05 18:48:16 +0300

    bump nixpkgs

--- rebuild.sh
+++ rebuild.sh
@@ -1,4 +1,5 @@
-pinnedCommit="3c5319ad3aa51551182ac82ea17ab1c6b0f0df89"
+# Commit comes from nixos-unstable branch
+pinnedCommit="78419edadf0fabbe5618643bd850b2f2198ed060"
 nixpkgsUrlFromCommit="https://github.com/NixOS/nixpkgs/archive/$pinnedCommit.tar.gz"
 
 NIX_PATH="nixpkgs=$nixpkgsUrlFromCommit" sudo -E nixos-rebuild switch -I nixos-config=configuration.nix "$@"
