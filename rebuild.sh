3fc2205d693407426e731b81684211e0ccbdb0e2
Jussi Kuokkanen <jussi.kuokkanen@protonmail.com>
2023-07-05 18:47:44 +0300

    propagate arguments to nixos-rebuild

--- rebuild.sh
+++ rebuild.sh
@@ -1,5 +1,4 @@
 pinnedCommit="3c5319ad3aa51551182ac82ea17ab1c6b0f0df89"
 nixpkgsUrlFromCommit="https://github.com/NixOS/nixpkgs/archive/$pinnedCommit.tar.gz"
 
-
-NIX_PATH="nixpkgs=$nixpkgsUrlFromCommit" sudo -E nixos-rebuild switch -I nixos-config=configuration.nix
+NIX_PATH="nixpkgs=$nixpkgsUrlFromCommit" sudo -E nixos-rebuild switch -I nixos-config=configuration.nix "$@"
